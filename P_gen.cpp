253368d8bc9cbf009f2b9b7f01f2407dc70dc5a0
aurooura <auro.soares@xmobots.com.br>
2021-02-18 19:32:11 -0300

    P

--- P_gen.cpp
+++ P_gen.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 
-int main() {
-    string func = "SelectMagFusion";
-    string model = "SM";
-    int sm = 29;
-    int start = 7;
+int main(int argc, char *argv[]) {
+    string func = argv[1];
+    string model = argv[2];
+    int sm = 0;
+    int start = 0;
     int md = 0;
     for(int i = 0; i < 24; ++i) {
         for(int j = 0; j < 24; ++j) {
             if(md == 0 || md%15 == 0) {
                 if(md) cout << "END\n";
-                cout << model << "_" << sm << " SM" << sm << endl;
+                cout << model << "_" << sm << " " << model << sm << endl;
                 cout << func << start++ << endl;
                 sm++;
             }
