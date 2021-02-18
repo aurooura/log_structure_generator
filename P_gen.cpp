fe4822d22caa605a1230297c23fade05de89522f
aurooura <auro.soares@xmobots.com.br>
2021-02-18 18:37:11 -0300

    impoprovement

--- P_gen.cpp
+++ P_gen.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 
 int main() {
-    string func = "alignMagStateDeclination";
-    string model = "AD";
+    string func = "SelectMagFusion";
+    string model = "SM";
     int sm = 29;
     int start = 7;
     int md = 0;
@@ -14,7 +14,7 @@ int main() {
         for(int j = 0; j < 24; ++j) {
             if(md == 0 || md%15 == 0) {
                 if(md) cout << "END\n";
-                cout << model << "_" << sm << " AD" << sm << endl;
+                cout << model << "_" << sm << " SM" << sm << endl;
                 cout << func << start++ << endl;
                 sm++;
             }
---
+++ b.py
@@ -0,0 +1,17 @@
+
+
+
+for i in range(0, 28):
+    print("float storedOutput[{0}].quat.q1".format(i))
+    print("float storedOutput[{0}].quat.q2".format(i))
+    print("float storedOutput[{0}].quat.q3".format(i))
+    print("float storedOutput[{0}].quat.q4".format(i))
+    print("float storedOutput[{0}].velocity[0]".format(i))
+    print("float storedOutput[{0}].velocity[1]".format(i))
+    print("float storedOutput[{0}].velocity[2]".format(i))
+    print("float storedOutput[{0}].position[0]".format(i))
+    print("float storedOutput[{0}].position[1]".format(i))
+    print("float storedOutput[{0}].position[2]".format(i))
+
+
+
